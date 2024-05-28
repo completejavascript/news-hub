652746b360a766baf90f91d523b9cc44659e8391
Lam Pham <completejavascript.super@gmail.com>
2024-05-28 14:52:16 +0700

    fix: remove not use & implement header with toggle theme

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,70 @@
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+import {ref} from 'vue';
+
+import BrandName from '@/components/BrandName.vue';
+import IconMoon from '@/components/icons/IconMoon.vue';
+import IconSun from '@/components/icons/IconSun.vue';
+import {THEME_KEY} from './config';
+import type {Theme} from './types';
+
+function setTheme(theme: Theme) {
+  localStorage.setItem(THEME_KEY, theme);
+  currentTheme.value = theme;
+
+  if (theme === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+}
+
+function getStorageTheme(): Theme {
+  return localStorage.getItem(THEME_KEY) as Theme;
+}
+
+function getMediaPreference(): Theme {
+  const hasDarkPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (hasDarkPreference) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
+
+function toggleTheme() {
+  if (currentTheme.value === 'light') {
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+}
+
+const currentTheme = ref<Theme>(getStorageTheme() ?? getMediaPreference());
+const isNavBarOpen = ref(false);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div className="h-screen bg-white dark:bg-slate-900 text-slate-400 antialiased relative">
+    <div v-if="!isNavBarOpen">
+      <div
+        className="sticky top-0 z-40 py-4 px-8 border-b border-slate-900/10 dark:border-slate-300/10 flex justify-between items-center"
+      >
+        <BrandName />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <div className="flex gap-5">
+          <button
+            className="w-5 h-5 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"
+            @click="toggleTheme"
+          >
+            <template v-if="currentTheme === 'light'">
+              <IconSun />
+            </template>
+            <template v-else>
+              <IconMoon />
+            </template>
+          </button>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/BrandName.vue
@@ -0,0 +1,8 @@
+<template>
+  <div className="flex gap-4">
+    <img src="@/assets/logo.svg" alt="" width="32" height="32" />
+    <h2 className="hidden md:block text-xl text-slate-900 dark:text-slate-100 font-semibold">
+      News Hub
+    </h2>
+  </div>
+</template>
