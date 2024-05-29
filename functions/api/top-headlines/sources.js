f6362beb9a80b576348f345742ea033436579924
Lam Pham <completejavascript.super@gmail.com>
2024-05-29 19:21:07 +0700

    test functions

--- functions/api/top-headlines/sources.js
+++ functions/api/top-headlines/sources.js
@@ -1,10 +1,23 @@
-export default {
-  async fetch(request, env) {
-    const proxyRequest = new Request(
-      `https://newsapi.org/v2/top-headlines/sources?apiKey=${env.VITE_API_KEY}`,
-      request
+export async function onRequestGet(context) {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines/sources?apiKey=${context.env.VITE_API_KEY}`
     );
-    const proxyResponse = await fetch(proxyRequest);
-    return proxyResponse;
-  },
-};
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({error: 'Error fetching the requested URL'}), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  }
+}
