8be45a16f614060834db036fb78733cbc83ee146
Lam Pham <completejavascript.super@gmail.com>
2024-05-29 17:34:47 +0700

    test worker

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const THEME_KEY = 'theme';
-export const HOST_API = 'https://cors-workers.pages.dev';
+export const HOST_API = 'https://news-hub.pages.dev';
 export const NEWS_API = 'https://newsapi.org/v2';
 export const API_KEY = import.meta.env.VITE_API_KEY;
