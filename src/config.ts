368b7e6003991bdbb78c1aec61ffd8ab0a567eeb
Lam Pham <completejavascript.super@gmail.com>
2024-05-29 15:10:18 +0700

    fix: define VITE env for API_KEY

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const THEME_KEY = 'theme';
-
 export const HOST_API = 'https://newsapi.org/v2/';
-export const API_KEY = '451f7dc008e74f8cbf038779dd8bdec9'; // develop only
+export const API_KEY = import.meta.env.VITE_API_KEY;
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VUE_APP_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
