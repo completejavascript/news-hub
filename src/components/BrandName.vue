c59652f0e29cf35f5fb1b44c62082e30c4aa9c1d
Lam Pham <completejavascript.super@gmail.com>
2024-05-30 10:36:54 +0700

    fix: add router link to brand name

--- src/components/BrandName.vue
+++ src/components/BrandName.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import {RouterLink} from 'vue-router';
+</script>
+
 <template>
-  <div class="flex gap-4 items-center">
-    <img src="@/assets/logo.svg" alt="" width="32" height="32" />
-    <h2 class="hidden md:block text-xl text-slate-900 dark:text-slate-100 font-semibold">
-      News Hub
-    </h2>
-  </div>
+  <RouterLink to="/">
+    <div class="flex gap-4 items-center">
+      <img src="@/assets/logo.svg" alt="" width="32" height="32" />
+      <h2 class="hidden md:block text-xl text-slate-900 dark:text-slate-100 font-semibold">
+        News Hub
+      </h2>
+    </div>
+  </RouterLink>
 </template>
